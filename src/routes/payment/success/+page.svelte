f02eb831a4b4c37eac2f06fc2fea0f40d3822524
Harris Dzulkarnain <harisidzwan@gmail.com>
2023-09-15 15:18:29 +0800

    Working Website

--- src/routes/payment/success/+page.svelte
+++ src/routes/payment/success/+page.svelte
@@ -1,11 +1,14 @@
 <head>
-  <title>Thanks for your order!</title>
+  <title>Thank you for your purchase!</title>
 </head>
-<body>
-  <section>
-    <p>
-      We appreciate your business! If you have any questions, please email
-      <a href="mailto:orders@example.com">orders@example.com</a>.
-    </p>
+
+<body class="bg-black-100 flex items-center justify-center ">
+  <section class="bg-white p-8 rounded-lg shadow-lg max-w-md text-center mt-5">
+      <h1 class="text-2xl text-black font-bold mb-4">Thanks for your purchase!</h1>
+      <p class="text-lg text-black mb-4">For any inquiries, please contact us at:</p>
+      <a href="mailto:hello@next.com" class="text-blue-500 hover:underline">hello@next.com</a>
   </section>
 </body>
+
+
+
